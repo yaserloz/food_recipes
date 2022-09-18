35ecd348107f01e7828485f51ebf0d572411bee1
ALQAISI yasir <yasir.alqaisi.ext@up.coop>
2022-09-18 11:27:41 +0200

    updates

--- pages/[...params].tsx
+++ pages/[...params].tsx
@@ -3,14 +3,14 @@ import React, { useContext } from "react";
 import AppContext from "../components/AppContext";
 import Nav from "../components/Nav";
 
-export const getServerSideProps = async (context) => {
-  const response =  await axios(`/recipes/1`)
-  const recipe = response.data;
-  return {props:{recipe}};
-};
+// export const getServerSideProps = async (context) => {
+//   const response =  await axios(`/recipes/1`)
+//   const recipe = response.data;
+//   return {props:{recipe}};
+// };
 
-export default function recipe({recipe}) {
-  console.log(recipe)
+export default function recipe() {
+  // console.log(recipe)
     return (
     <>
       <Nav />
