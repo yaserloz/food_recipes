dc24f078248ae31d4b7027d10de4b2f88c12ded4
ALQAISI yasir <yasir.alqaisi.ext@up.coop>
2022-09-18 11:42:51 +0200

    updates

--- middleware.ts
+++ middleware.ts
@@ -4,9 +4,9 @@ import { getCookie } from 'cookies-next'
 
 export function middleware(request: NextRequest) {   
   const url = request.nextUrl.clone()   
-  // if (url.pathname === '/') {
-  //   const lang = request.cookies.get("lang") ?? 'en'
-  //   url.pathname = '/'+lang+'/recipes'
+  if (url.pathname === '/') {
+    const lang = request.cookies.get("lang") ?? 'en'
+    url.pathname = '/'+lang+'/recipes'
     return NextResponse.redirect(url)   
   } 
 }
--- next.config.js
+++ next.config.js
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-  ignoreDuringBuilds: true,
+  swcMinify: false,
   typescript: {
     ignoreBuildErrors: true,
   },
